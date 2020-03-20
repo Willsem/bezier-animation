18ef10fd6af692672ccbef3de2cb2b7bff542625
NastyRu <nasty.sidenko@mail.ru>
2020-03-20 15:36:46 +0300

    Added brows

--- src/source/Commands/Draw.cc
+++ src/source/Commands/Draw.cc
@@ -5,4 +5,16 @@ Draw::Draw(OpenGLWidget &widget)
 
 void Draw::execute(Facade &facade)
 {
+    OpenGLDrawer drawer(_widget);
+
+    facade.LeftBrow.setP1(10, 100);
+    facade.LeftBrow.setP2(60, 50);
+    facade.LeftBrow.setP3(110, 100);
+
+    facade.RightBrow.setP1(130, 100);
+    facade.RightBrow.setP2(180, 50);
+    facade.RightBrow.setP3(230, 100);
+
+    facade.getDrawManager().drawBezier(facade.RightBrow.BezierList(), drawer);
+    facade.getDrawManager().drawBezier(facade.LeftBrow.BezierList(), drawer);
 }
