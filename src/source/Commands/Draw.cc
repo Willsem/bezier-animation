5c323619ed72f8906bf6fe91023c931d1712ecfc
NastyRu <nasty.sidenko@mail.ru>
2020-03-20 16:05:03 +0300

    Adde new coords

--- src/source/Commands/Draw.cc
+++ src/source/Commands/Draw.cc
@@ -7,13 +7,13 @@ void Draw::execute(Facade &facade)
 {
     OpenGLDrawer drawer(_widget);
 
-    facade.LeftBrow.setP1(10, 100);
-    facade.LeftBrow.setP2(60, 50);
-    facade.LeftBrow.setP3(110, 100);
+    facade.LeftBrow.setP1(0, 100);
+    facade.LeftBrow.setP2(40, 50);
+    facade.LeftBrow.setP3(80, 100);
 
-    facade.RightBrow.setP1(130, 100);
-    facade.RightBrow.setP2(180, 50);
-    facade.RightBrow.setP3(230, 100);
+    facade.RightBrow.setP1(100, 100);
+    facade.RightBrow.setP2(140, 50);
+    facade.RightBrow.setP3(180, 100);
 
     facade.getDrawManager().drawBezier(facade.RightBrow.BezierList(), drawer);
     facade.getDrawManager().drawBezier(facade.LeftBrow.BezierList(), drawer);
